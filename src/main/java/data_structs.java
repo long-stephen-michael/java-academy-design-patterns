e24c7634a29e6f52d01af52ac5d4df001d2c0c22
Marquis Chamberlain <marquischamberlain@gmail.com>
2021-01-28 19:28:38 -0500

    adding data-structure package

---
+++ src/main/java/data_structs.java
@@ -0,0 +1,14 @@
+public class data_structs {
+
+    public static void main (String args[]) {
+        /*
+            Linked List
+            Maps*
+            Trees
+
+         */
+
+
+
+    }
+}
